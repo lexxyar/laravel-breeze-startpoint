97eebd773cd758e27c48197840cb5d2ad7580247
Aleksey Komov <lexxyar@ya.ru>
2024-09-18 18:58:55 +0300

    auth routes to api routes too

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -12,6 +12,8 @@ Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
 
+require __DIR__ . '/auth.php';
+
 Route::middleware(['auth:sanctum'])->group(function () {
     Route::put('/profile', [ProfileController::class, 'update'])
         ->name('profile.update');
